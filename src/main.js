ec55d1f031794241876c114e4867e4745d936188
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-10-02 08:29:49 -0400

    chore: testing pr comment flow (#2)
    when we merge this. we should see a build action complete, re-use the CAR and upload context, and skip re-uploading

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ console.log('Static site loaded: hosted via Filecoin.')
 console.log('modifying site content to test PR comment update')
 
 console.log('2025-10-02 08:16 PST')
+
+console.log('2025-10-02 08:23 PST: test PR workflow with Filecoin upload action')
