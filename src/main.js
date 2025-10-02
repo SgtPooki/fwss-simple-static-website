5269052253a1f8adaad3dc9b0a4c3c961250482e
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-10-02 08:15:16 -0400

    chore: change build assets

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 console.log('Static site loaded: hosted via Filecoin.')
 
 console.log('modifying site content to test PR comment update')
-
-console.log('modifying site content to test PR comment update 2')
