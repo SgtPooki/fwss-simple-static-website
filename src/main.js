c6bfd3b28538a4d9202c71a2e5fd21f3927b8ac0
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-10-02 08:59:13 -0400

    chore: testing pr comment and main branch upload (#3)

--- src/main.js
+++ src/main.js
@@ -5,3 +5,6 @@ console.log('modifying site content to test PR comment update')
 console.log('2025-10-02 08:16 PST')
 
 console.log('2025-10-02 08:23 PST: test PR workflow with Filecoin upload action')
+
+
+console.log('2025-10-02 08:50 PST: test PR workflow with Filecoin upload action. final summary of main branch upload action should have details')
